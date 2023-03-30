fdd2e22424f2aa555f53e5ea935ed00941c5dd1a
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-30 15:06:00 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string frasedigitada, frasetonervoso, nome;
+Console.Write("Olá, qual o seu nome?"); nome = Console.ReadLine()!;
+Console.Write($"{nome}, me diga o que vc acha desse programa..."); frasedigitada = Console.ReadLine()!;
+
+frasetonervoso = $"{frasedigitada.Remove(10)}... NÂO, PERA! Tô nervoso";
+Console.WriteLine(frasetonervoso);
